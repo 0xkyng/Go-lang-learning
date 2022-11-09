aa54d5be85e00a1d4dc2682d4538ec6fa5d42cf2
Isaac Wanger <teryima127@gmail.com>
2022-11-09 15:24:35 +0100

    learnt structs

--- main.go
+++ main.go
@@ -1,27 +1,27 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func main() {
-	fmt.Println("Hello, world!")
+// func main() {
+// 	fmt.Println("Hello, world!")
 
-	var whatToSay string 
+// 	var whatToSay string 
 
-	var integer int
+// 	var integer int
 
-	whatToSay = "Goodbye, good world"
+// 	whatToSay = "Goodbye, good world"
 
-	fmt.Println(whatToSay)
+// 	fmt.Println(whatToSay)
 
-	integer = 7
-	fmt.Println("integer is set to", integer)
+// 	integer = 7
+// 	fmt.Println("integer is set to", integer)
 
-	whatWasSaid, theOtherThingThatWasSaid := saySomething()
-	fmt.Println("The function returned", whatWasSaid, theOtherThingThatWasSaid)
+// 	whatWasSaid, theOtherThingThatWasSaid := saySomething()
+// 	fmt.Println("The function returned", whatWasSaid, theOtherThingThatWasSaid)
 
 
 		
-};
-func saySomething() (string, string) {
-	return "something", "else"
-}
+// };
+// func saySomething() (string, string) {
+// 	return "something", "else"
+// }
---
+++ struct.go
@@ -0,0 +1,32 @@
+// package main
+
+// import (
+// 	"log"
+// 	"time"
+// )
+
+// type User struct {
+// 	FirstName   string
+// 	LastName    string
+// 	PhoneNumber string
+// 	Age         int
+// 	BirthDate   time.Time
+// }
+
+
+// func main() {
+// 	user:= User {
+// 		FirstName: "Isaac",
+// 		LastName: "Wanger",
+// 		PhoneNumber: "+234-080-69126463",
+
+// 	}
+
+// 	log.Println(user.FirstName, user.LastName)
+// }
+
+
+// // A fauction and a variable beginning with a  capital letter can accessed outside the current package
+// func Whatever() {
+
+// };
