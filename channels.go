90178f3035dfbb593d180bdcf71f994140fe4ff3
Isaac Wanger <teryima127@gmail.com>
2022-11-10 19:40:50 +0100

    Learnt channels

---
+++ channels.go
@@ -0,0 +1,17 @@
+package main
+
+
+func CalculateValue(intChan chan int) {
+	
+}
+
+func main() {
+
+// syntax for channels
+// channel_name := make(chan val-type)
+
+// creating channel
+intChan := make(chan int)
+
+
+}
